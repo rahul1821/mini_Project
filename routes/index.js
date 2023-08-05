4f524ca5284a9d28626a8a68353100f2f7e6f2ce
Rahul Singh <rs388903@gmail.com>
2023-08-05 17:36:14 +0530

    init

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,5 @@ const homeController = require('../controllers/home_controller');
 
 console.log("It's Router Folder");
 router.get('/', homeController.home);
+router.use('/users',require('./users'));
 module.exports = router;
